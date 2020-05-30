effc52960ac465a58ef69f517d4200b29b5ca20a
Yu-Ting Huang <ythuang@simba-fe.iel.epfl.ch>
2020-05-30 15:31:35 +0200

    update

--- .taskfarm_job_187554/0.sh
+++
@@ -1,1 +0,0 @@
-python3.6 main.py --save_model --env InvertedPendulum-v2 --expl_noise 0.01 --optimizer SGLD --epsilon 0.001 --two_player False --alpha 0.0 --seed 0
--- .taskfarm_job_187558/0.sh
+++
@@ -1,1 +0,0 @@
-python3.6 test.py --env InvertedPendulum-v2 --eval_type model --optimizer RMSprop --action_noise action_noise_0.1 --alpha 0.0 --two_player False
